b1aab9c89174b871592d12f255e2ad4cb62bd37d
Trần Xuân Trường <techoigame437@gmail.com>
2026-03-05 02:39:06 +0700

    add auto sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
-  plugins: [react()]
-});
+  plugins: [
+    sitemap({
+      hostname: 'https://mytools-9ns.pages.dev'
+    })
+  ]
+})
