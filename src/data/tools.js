36de28a1e14f171c76036dae987e85eebc302c12
Trần Xuân Trường <techoigame437@gmail.com>
2026-03-18 06:40:05 +0700

    Fix status value in tools data
    First YouTube download version released.

--- src/data/tools.js
+++ src/data/tools.js
@@ -6,7 +6,7 @@ export const tools = [
     version: "v1.0.0",
     size: "28MB",
     os: "Windows 10/11",
-    status: "//released", // 👈 đã phát hành
+    status: "released", // 👈 đã phát hành
     downloadUrl:
       "https://github.com/TranXuanTruong-BTEC/media-desktop-app/releases/download/MediaDesktopApp-Setup.exe",
     image: "/images/yt2.png",
