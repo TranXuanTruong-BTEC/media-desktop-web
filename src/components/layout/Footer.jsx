10ef931e73496b86fbf0303655ac4b99fa65030f
Trần Xuân Trường <techoigame437@gmail.com>
2026-03-19 09:45:57 +0700

    sửa laị tiêu đề thành Tools

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer className="border-t border-white/10 mt-20 py-10 text-center text-sm text-gray-400">
-      <p>© 2026 MediaTools. All rights reserved.</p>
+      <p>© 2026 Tools. All rights reserved.</p>
     </footer>
   );
 }
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
     <header className="py-6 border-b border-white/10">
       <div className="max-w-6xl mx-auto text-center">
         <Link to="/" className="text-xl font-semibold">
-          Media<span className="text-blue-400">Tools</span>
+          <span className="text-blue-400">Tools</span>
         </Link>
       </div>
     </header>
